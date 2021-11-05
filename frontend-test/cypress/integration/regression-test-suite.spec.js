9848da8b7b15ae2a48ece2274011a32588dec73d
Sagal Jama <sagal_j@hotmail.com>
2021-11-05 02:46:21 +0100

    Test project

---
+++ frontend-test/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+// This is a test suite
+describe('Regression test suite', function(){
+
+    // This is a test case
+    it ('Perform valid login', function(){
+        cy.visit(' http://localhost:3000')
+        
+
+
+
+    })
+
+})
