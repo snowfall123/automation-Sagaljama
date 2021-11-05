50383a206c01783b45ea8f39f60173620e028cc0
Sagal Jama <sagal_j@hotmail.com>
2021-11-05 15:48:09 +0100

    init

--- frontend-test/cypress/integration/regression-test-suite.spec.js
+++ frontend-test/cypress/integration/regression-test-suite.spec.js
@@ -6,7 +6,27 @@ describe('Regression test suite', function(){
     // This is a test case
     it ('Perform valid login', function(){
         cy.visit(' http://localhost:3000')
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.get(':nth-child(4) > .btn').click()
+        cy.get('h2 > .btn').click()
+        cy.get(':nth-child(1) > input').type('2021-09-05')
+        cy.get(':nth-child(2) > input').type('2021-09-08')
+        cy.get(':nth-child(3) > select').select("2")
+        cy.get(':nth-child(4) > select').select('1')
+        cy.get(':nth-child(5) > select').select('1')
+        cy.get('.blue').click()
         
+
+       
+
+        
+
+
+
+
 
 
 
